f491753f83b047fcd63f87a18816ce52163b3751
Cheryl Feng <cherylfeng@VEI-MacBookPro12.local>
2020-11-09 14:39:27 -0800

    added rmd files

---
+++ analysis.R
@@ -0,0 +1,23 @@
+library("dplyr")
+hofstede <- read.csv("https://geerthofstede.com/wp-content/uploads/2016/08/6-dimensions-for-website-2015-08-16.csv",
+                     stringsAsFactors = FALSE,
+                     sep = ";")
+idv_data_df <- hofstede %>% 
+  select(country, idv) %>%
+  mutate(idv = as.numeric(idv)) %>%
+  filter(!is.na(idv)) %>%
+  arrange(idv)
+
+idv_summary2_df <- idv_data_df %>%
+  summarise(avg_idv = mean(idv),
+            min_idv = min(idv),
+            max_idv = max(idv))
+
+max_country <- idv_data_df %>%
+  filter(idv == max(idv)) %>%
+  pull(country)
+max_country
+
+min_country <- idv_data_df %>%
+  filter(idv == min(idv)) %>%
+  pull(country)
